6b584f4461a07a8bb20ba9777bf37e051316ea1b
lourw <louieweng@outlook.com>
2023-01-27 16:26:05 -0800

    initial commit to set up part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).unwrap();
+
+    println!("input is: {}", input)
+}
+
+fn read_user_input() -> String {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    return input
 }
