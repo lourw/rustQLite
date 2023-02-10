a53b4c496d4fa4d5233b330f959ab7b1a5263396
Louie Weng <louieweng@outlook.com>
2023-02-10 14:07:10 -0800

    complete repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::{io::{self, Write}, process};
+
 fn main() {
     println!("Hello, world!");
+    loop {
+        print_prompt();
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line.");
+
+        if input.trim() == ".exit" {
+            println!("Exiting from process...");
+            process::exit(0x0100);
+        } else {
+            println!("{}", input);
+        }
+    }
+}
+
+fn print_prompt() {
+    print!("db > ");
 }
